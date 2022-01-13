20ae1dbd1a22d7384e25122deb46b93dde7f0fde
FabioUras <urasfabio94@gmail.com>
2022-01-13 19:15:36 +0100

    abbiamo installato composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fed500d06eec0f1a9ec2c86f716a4fbb29c0884e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fed500d06eec0f1a9ec2c86f716a4fbb29c0884e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
