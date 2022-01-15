569af0229f221b8cbc460dad207c9e483ae2c07d
FabioUras <urasfabio94@gmail.com>
2022-01-15 11:52:19 +0100

    Connessione al database tramite istanza di PDO

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
-echo 'Hello World';
+try
+
+{
+
+$pdo= new PDO("mysql:host=127.0.0.1;dbname=differenziata",'root','');
+
+$statement= $pdo->prepare("SELECT * FROM settimana");
+
+$statement->execute();
+
+var_dump($statement->fetchAll(PDO::FETCH_OBJ));
+
+
+}catch (PDOException){
+
+  die('Could not connect');
+
+}
+
+
+
 
 ?>
